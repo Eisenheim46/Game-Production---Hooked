efb711fccf3565f6077cc5470f71a3d26c1b0a34
Michail Michail <michaelmich2@gmail.com>
2018-10-23 21:50:53 -0500

    KillZone

---
+++ Assets/Deathzone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathzone : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform checkpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.position = checkpoint.position;
+        }
+    }
+}
