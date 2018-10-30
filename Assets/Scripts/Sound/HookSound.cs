8764272089865f75e35d09d1e0b666b358f16ef3
Anthony Flores <Anthony.Flores2@iam.local>
2018-10-30 12:13:43 -0500

    sound and tag fixes

---
+++ Assets/Scripts/Sound/HookSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookSound : MonoBehaviour {
+
+    [SerializeField] private AudioClip woodClip;
+    [SerializeField] private AudioClip metalClip;
+    [SerializeField] private AudioClip doorClip;
+    [SerializeField] private AudioClip gateClip;
+
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wood")
+        {
+            audio.clip = woodClip;
+            audio.PlayDelayed(0);
+        }
+        if (collision.gameObject.tag == "Metal")
+        {
+            audio.clip = metalClip;
+            audio.PlayDelayed(0);
+        }
+        if (collision.gameObject.tag == "door")
+        {
+            audio.clip = doorClip;
+            audio.PlayDelayed(0);
+        }
+        if (collision.gameObject.tag == "gate")
+        {
+            audio.clip = gateClip;
+            audio.PlayDelayed(0);
+        }
+    }
+}
