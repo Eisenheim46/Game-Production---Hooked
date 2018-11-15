9f9acec942f17972ac57ef83dd5dadf861bcc69f
Eisenheim46 <32074088+Eisenheim46@users.noreply.github.com>
2018-11-15 15:37:53 -0600

    Channges on the main

--- Assets/Scripts/EnableChildPhysics.cs
+++ Assets/Scripts/EnableChildPhysics.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnablePhysics : MonoBehaviour {
+public class EnableChildPhysics : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
---
+++ Assets/Scripts/EndZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndZone : MonoBehaviour {
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        SceneManager.LoadScene("EndScene");
+
+    }
+
+
+
+}
