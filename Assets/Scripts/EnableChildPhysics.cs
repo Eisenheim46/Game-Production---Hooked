59922f9f3aaafce8ba5672a9c83bed22d2e6e4fd
David Beaudoin <David.Beaudoin@iam.local>
2018-11-15 14:43:29 -0600

    Add EnableChildPhysics script for falling objects

---
+++ Assets/Scripts/EnableChildPhysics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePhysics : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach(Transform child in transform)
+            {
+                child.GetComponent<Rigidbody>().isKinematic = false;
+                child.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+}
