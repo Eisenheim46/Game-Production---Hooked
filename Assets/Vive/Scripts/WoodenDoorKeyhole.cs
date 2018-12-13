14a53507b8930d8c685bcffa30ec06f2bc8b051d
michaelmich3 <michaelmich3@hotmail.com>
2018-12-13 10:34:40 -0600

    Added Key functionality

---
+++ Assets/Vive/Scripts/WoodenDoorKeyhole.cs
@@ -0,0 +1,39 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenDoorKeyhole : MonoBehaviour
+{
+    [SerializeField] private GameObject woodenDoor;
+    [SerializeField] private bool startsLocked;
+
+    private bool isUnlocked;
+
+	void Start ()
+    {
+        if (startsLocked)
+            isUnlocked = false;
+        LockUnlock();
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Key")
+        {
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.transform.position = transform.position;
+            other.transform.rotation = transform.rotation;
+            other.GetComponent<Pickable>().enabled = false;
+            LockUnlock();
+        }
+    }
+
+    private void LockUnlock()
+    {
+        if (isUnlocked)
+            woodenDoor.GetComponent<Rigidbody>().isKinematic = false;
+        else
+            woodenDoor.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
